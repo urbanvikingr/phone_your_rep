d62d7a697238b0611840513cba9e89ec827e4458
Garen Vartanian <vartanian.garen@yahoo.com>
2017-01-15 13:52:15 -0500

    aligned address and fax

--- script/ReportButton.js
+++ script/ReportButton.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 export default class ReportButton extends React.Component {
 	render() {
-		function nothingToSend() {
-	var phone = document.getElementById("number");
-	var location = document.getElementById("office");
-	var vCard = document.getElementById("trouble");
-	var email = document.getElementById("incorrect");
+	function nothingToSend() {
+		var phone = document.getElementById("number");
+		var location = document.getElementById("office");
+		var vCard = document.getElementById("trouble");
+		var email = document.getElementById("incorrect");
 
 	if(phone.checked == false && location.checked == false && vCard.checked == false && email.checked == false) {
 		alert("It looks like there's no issue for you to send us!");
 	}
 }
 		return(
-		<div>
+		<div className="floatingDiv">
 		<button type="button" className="btn btn-info btn-md" data-toggle="modal" data-target="#reportIssue">Report Issue</button>
 		<div className="modal fade" id="reportIssue" role="dialog">
-		<div className="modal-dialog">
-		<div className="modal-content">
-		<div className="modal-header">
-		<button type="button" className="close" data-dismiss="modal">&times;</button>
-		<h4 className="modal-title">Report an issue</h4>
-		<div className="modal-body">
-		<p>Which information is wrong?</p>
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+							<button type="button" className="close" data-dismiss="modal">&times;</button>
+							 <h4 className="modal-title">Report an issue</h4>
+							<div className="modal-body">
+					<p>Which information is wrong?</p>
 						<div className="radio">
 							<label><input type="checkbox" id="number" name="phone" /> Incorrect Phone Number</label>
 						</div>
@@ -43,13 +43,12 @@ export default class ReportButton extends React.Component {
 							<button type="button" id="nothingToSend" className="btn btn-default" data-dismiss="modal" onClick={nothingToSend}>Send</button>
 						</div>
 
+					</div>
+				</div>
+	   	</div>
 		</div>
-		</div>
-		</div>
-		</div>
-		</div>
-		</div>
-
+	</div>
+</div>
 		)
 	}
 }
