ee411bc4fe3360d95ad68a1ed3e39f354ae67700
Kyle Butts <buttskyle96@gmail.com>
2017-02-02 11:18:41 -0800

    add static sites

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp')
+var handlebars = require('gulp-compile-handlebars')
+var rename = require('gulp-rename')
+var reps = require('./app/reps/reps.json')
+
+gulp.task('handlebars', function() {
+    for (var i = 0; i < reps.length; i++) {
+        var rep = reps[i];
+        var folderName = rep.first.toLowerCase() + '_' + rep.last.toLowerCase()
+
+
+
+
+        gulp.src('app/templates/rep.handlebars')
+            .pipe(handlebars(rep))
+            .pipe(rename('index.html'))
+            .pipe(gulp.dest('docs/reps/' + folderName + '/'));
+    }
+});
+
+
+gulp.task('default', ['handlebars']);
--- webpack.production.js
+++ webpack.production.js
@@ -3,16 +3,16 @@ const path = require('path');
 const webpackConfig = require('./webpack.config');
 
 module.exports = Object.assign({}, webpackConfig, {
-  output: {
-    path: path.join(path.resolve(), 'docs'),
-    filename: "[name].js"
-  },
-  devtool: 'cheap-module-source-map',
-  plugins: webpackConfig.plugins.concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ])
+    output: {
+        path: path.join(path.resolve(), 'docs'),
+        filename: "[name].js"
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: webpackConfig.plugins.concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ])
 });
